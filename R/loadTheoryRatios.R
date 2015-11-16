8755ab7da2494bfe2fc2e0f4bc5e40c6b4a8b994
Witold Wolski <wewolski@gmail.com>
2015-11-16 10:47:54 +0100

    new columns added

--- R/loadTheoryRatios.R
+++ R/loadTheoryRatios.R
@@ -8,5 +8,9 @@ loadTheoryHLRatios <- function(){
   file <- file.path(path.package("p1755"),"extdata/theoryRatios.txt")
   ratios<-read.csv(file , stringsAsFactors = FALSE , header=FALSE,sep=" ")
   colnames(ratios)<-c("peptide","concentration","ratio")
+  ratios[,1] <- gsub("\\[|\\]","",ratios[,1])
+  ratios[,4] <- log2(ratios[,3])
+  colnames(ratios) <- c("Pepsequence", "concentration", "logfoldchange" , "log2fc")
+  ratios<-ratios[order(ratios$Pepsequence),]
   return(ratios)
 }
